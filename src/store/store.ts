dfd176aaf4f5ac10d18c22a291fb5f487b5002d2
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-05-11 16:54:38 +0200

    Imported store.ts

---
+++ src/store/store.ts
@@ -0,0 +1,16 @@
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import dishesCategoriesReducer from "../slices/dishesCategoriesSlice";
+import statisticsReducer from "../slices/statisticsSlice";
+
+const rootReducer = combineReducers({
+    dishesCategories: dishesCategoriesReducer,
+    statistics: statisticsReducer
+})
+
+export const store = configureStore({
+    reducer: rootReducer
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
