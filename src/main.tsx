179c661aded00f005b83c9d9f5de44accb13a591
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-05-11 16:55:13 +0200

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {Router} from "./routing/Router.tsx";
+import {Provider} from "react-redux";
+import {store} from "./store/store.ts";
+import {I18nextProvider} from "react-i18next";
+import i18n from "./i18n.ts"
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <I18nextProvider i18n={i18n}>
+        <Provider store={store}>
+            <Router/>
+        </Provider>
+    </I18nextProvider>
+);
