66fcc1ee248393e265bf415088b6cbd7f8daa57a
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-05-12 20:35:18 +0200

    Updated ImageSection to use new hook instead of imageName

--- src/app/menu/menu-item-details/ImageSection.tsx
+++ src/app/menu/menu-item-details/ImageSection.tsx
@@ -1,27 +1,28 @@
-import {imagesPath} from "../../../apiData";
+import {s3BucketUrl} from "../../../apiData";
 import {setMenuItem} from "../../../slices/dishesCategoriesSlice";
 import {useSelector} from "react-redux";
 import {ArrowLeftIcon} from "../../icons/ArrowLeftIcon.js";
 import {Img} from "react-image";
 import {LoadingSpinner} from "../../icons/LoadingSpinner.js";
 import {useAppDispatch} from "../../../hooks/hooks.ts";
+import {useImageExists} from "../../../hooks/useHasImage.ts";
 
 export const ImageSection = () => {
     const dispatch = useAppDispatch();
     const {menuItem} = useSelector<any, any>(state => state.dishesCategories.view);
-    const imgName = menuItem.imageName;
+    const hasImage = useImageExists(menuItem?.id);
 
     const hideDetails = () => {
         dispatch(setMenuItem(null));
     }
 
     const renderImage = () => {
-        if (!imgName) {
+        if (!hasImage) {
             return (<></>);
         }
         return (
             <div className={'details-image-container'}>
-                <Img src={imagesPath + imgName}
+                <Img src={`${s3BucketUrl}/${menuItem?.id}`}
                      loader={<LoadingSpinner customContainerStyle={{height: 'calc(100% - 470px)'}}/>}
                      className={'details-image'}
                 />
