69da2a1ba556c237c532d8796237a739e5a5f0ec
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-05-12 20:24:15 +0200

    Created useHasImage hook

---
+++ src/hooks/useHasImage.ts
@@ -0,0 +1,26 @@
+import {useEffect, useState} from "react";
+import {s3BucketUrl} from "../apiData.ts";
+
+export const useImageExists = (id: number | string) => {
+    const [exists, setExists] = useState<boolean | null>(null);
+
+    useEffect(() => {
+        let active = true;
+        const url = `${s3BucketUrl}/${id}`;
+        const img = new Image();
+
+        img.src = url;
+        img.onload = () => {
+            if (active) setExists(true);
+        };
+        img.onerror = () => {
+            if (active) setExists(false);
+        };
+
+        return () => {
+            active = false;
+        };
+    }, [id]);
+
+    return exists;
+}
