344351bbd45dd8e68f1b8b7d409b958775eadd94
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-05-12 20:25:06 +0200

    Updated MenuItemPosition to use new hook

--- src/app/menu/MenuItemPosition.tsx
+++ src/app/menu/MenuItemPosition.tsx
@@ -1,21 +1,23 @@
-import {useDispatch} from "react-redux";
 import {setMenuItem} from "../../slices/dishesCategoriesSlice";
 import {MenuItemImage} from "./MenuItemImage";
 import {MenuItemContent} from "./MenuItemContent";
 import type {MenuItem} from "../../interfaces/MenuItem.ts";
+import {useAppDispatch} from "../../hooks/hooks.ts";
+import {useImageExists} from "../../hooks/useHasImage.ts";
 
 type MenuItemPositionType = {
     menuItem: MenuItem
 }
 
 export const MenuItemPosition = ({menuItem}: MenuItemPositionType) => {
-    const dispatch = useDispatch();
+    const dispatch = useAppDispatch();
+    const hasImage = useImageExists(menuItem.id);
 
     return (
         <div className={'menu-item-container'} onClick={() => dispatch(setMenuItem(menuItem))}>
-            <div className={`menu-item-grid ${!menuItem.imageName ? 'no-photo' : ''}`}>
-                <MenuItemImage menuItem={menuItem}/>
-                <MenuItemContent menuItem={menuItem}/>
+            <div className={`menu-item-grid ${!hasImage ? 'no-photo' : ''}`}>
+                <MenuItemImage menuItem={menuItem} hasImage={hasImage}/>
+                <MenuItemContent menuItem={menuItem} hasImage={hasImage}/>
             </div>
         </div>
     );
