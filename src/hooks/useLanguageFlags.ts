e7bb6378a2da6c7d8ccbd7ec5aae020611f3a548
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-05-13 18:14:57 +0200

    Updated useLanguageFlags

--- src/hooks/useLanguageFlags.ts
+++ src/hooks/useLanguageFlags.ts
@@ -2,11 +2,11 @@ import type {Language} from "../interfaces/Language.ts";
 
 export const useLanguageFlags = (): Language[] => {
     return [
-        {id: 'PL', path: '/theme/icons/flags/pl.svg'},
-        {id: 'EN', path: '/theme/icons/flags/pl.svg'},
-        {id: 'GE', path: '/theme/icons/flags/pl.svg'},
-        {id: 'FR', path: '/theme/icons/flags/pl.svg'},
-        {id: 'ES', path: '/theme/icons/flags/pl.svg'},
-        {id: 'UK', path: '/theme/icons/flags/pl.svg'},
+        {id: 'PL', path: '/theme/icons/flags/pl-large.svg'},
+        {id: 'EN', path: '/theme/icons/flags/pl-large.svg'},
+        {id: 'GE', path: '/theme/icons/flags/pl-large.svg'},
+        {id: 'FR', path: '/theme/icons/flags/pl-large.svg'},
+        {id: 'ES', path: '/theme/icons/flags/pl-large.svg'},
+        {id: 'UK', path: '/theme/icons/flags/pl-large.svg'},
     ];
 }
