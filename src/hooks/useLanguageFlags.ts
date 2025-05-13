d2d379d352a0f4f426092b24a53c0e943d1e2733
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-05-13 16:59:01 +0200

    Created useLanguageFlags

---
+++ src/hooks/useLanguageFlags.ts
@@ -0,0 +1,12 @@
+import type {Language} from "../interfaces/Language.ts";
+
+export const useLanguageFlags = (): Language[] => {
+    return [
+        {id: 'PL', path: '/theme/icons/flags/pl.svg'},
+        {id: 'EN', path: '/theme/icons/flags/pl.svg'},
+        {id: 'GE', path: '/theme/icons/flags/pl.svg'},
+        {id: 'FR', path: '/theme/icons/flags/pl.svg'},
+        {id: 'ES', path: '/theme/icons/flags/pl.svg'},
+        {id: 'UK', path: '/theme/icons/flags/pl.svg'},
+    ];
+}
