57399639eab19dc222e7ac4055b545abe4832c79
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-05-13 18:18:34 +0200

    Updated SearchIcon

--- src/app/icons/SearchIcon.tsx
+++ src/app/icons/SearchIcon.tsx
@@ -1,12 +1,11 @@
 export const SearchIcon = () => {
     return (
-        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g id="Group 645">
-                <circle id="Ellipse 119" cx="5.725" cy="5.7252" r="5.525" fill="black"/>
-                <circle id="Ellipse 118" cx="5.72498" cy="5.72539" r="3.825" fill="white"/>
-                <rect id="Rectangle 3851" x="7.84999" y="9.05225" width="1.7" height="8.25279" rx="0.85"
-                      transform="rotate(-45 7.84999 9.05225)" fill="black"/>
-            </g>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+            <path
+                d="M14 14L11.1333 11.1333M12.6667 7.33333C12.6667 10.2789 10.2789 12.6667 7.33333 12.6667C4.38781 12.6667 2 10.2789 2 7.33333C2 4.38781 4.38781 2 7.33333 2C10.2789 2 12.6667 4.38781 12.6667 7.33333Z"
+                stroke="black"
+                stroke-linecap="round"
+                stroke-linejoin="round"/>
         </svg>
     );
 }
