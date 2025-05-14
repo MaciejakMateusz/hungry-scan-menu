093630d2b9611b86868f972e751ac50dcd3c4da5
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-05-14 13:23:45 +0200

    Updated MenuItem interface

--- src/interfaces/MenuItem.ts
+++ src/interfaces/MenuItem.ts
@@ -1,11 +1,20 @@
 import type {Translatable} from "./Translatable.ts";
+import type {Label} from "./Label.ts";
+import type {Allergen} from "./Allergen.ts";
+import type {Ingredient} from "./Ingredient.ts";
+import type {Variant} from "./Variant.ts";
+import type {Banner} from "./Banner.ts";
 
 export interface MenuItem {
     id: number;
     name: Translatable;
     description: Translatable;
+    categoryId: number;
     price: number;
-    imageName: string;
-    bestseller: boolean;
-    new: boolean;
+    promoPrice: number;
+    labels: Label[];
+    allergens: Allergen[];
+    additionalIngredients: Ingredient[];
+    variants: Variant[];
+    banners: Banner[];
 }
