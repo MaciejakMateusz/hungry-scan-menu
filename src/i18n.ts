3721e21f64b358f96054b2c9602b3aaddb6a2b2a
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-05-14 13:22:58 +0200

    Updated list of supported languages in i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,9 +1,13 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import {initReactI18next} from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 import translation_en from './locales/en/translation_en.json';
 import translation_pl from './locales/pl/translation_pl.json';
+import translation_es from './locales/es/translation_es.json';
+import translation_de from './locales/de/translation_de.json';
+import translation_fr from './locales/fr/translation_fr.json';
+import translation_uk from './locales/uk/translation_uk.json';
 
 const languageDetector = new LanguageDetector(null, {
     order: ['cookie', 'localStorage', 'navigator', 'htmlTag', 'path', 'subdomain'],
@@ -15,8 +19,12 @@ i18n
     .use(initReactI18next)
     .init({
         resources: {
-            en: { translation: translation_en },
-            pl: { translation: translation_pl },
+            en: {translation: translation_en},
+            pl: {translation: translation_pl},
+            es: {translation: translation_es},
+            de: {translation: translation_de},
+            fr: {translation: translation_fr},
+            uk: {translation: translation_uk},
         },
         fallbackLng: 'pl',
         interpolation: {
