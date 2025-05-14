ac34a20bc6ee3ed3579700b20432c5bc053dc379
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-05-14 13:24:23 +0200

    Fixed tsx svg icons syntax

--- src/app/icons/SearchIcon.tsx
+++ src/app/icons/SearchIcon.tsx
@@ -4,8 +4,8 @@ export const SearchIcon = () => {
             <path
                 d="M14 14L11.1333 11.1333M12.6667 7.33333C12.6667 10.2789 10.2789 12.6667 7.33333 12.6667C4.38781 12.6667 2 10.2789 2 7.33333C2 4.38781 4.38781 2 7.33333 2C10.2789 2 12.6667 4.38781 12.6667 7.33333Z"
                 stroke="black"
-                stroke-linecap="round"
-                stroke-linejoin="round"/>
+                strokeLinecap="round"
+                strokeLinejoin="round"/>
         </svg>
     );
 }
