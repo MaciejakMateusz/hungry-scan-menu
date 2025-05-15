9e3ddc7efe32d54ddb7d96d7eb0f08bd759f2ed8
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-05-15 15:24:19 +0200

    Removed Labels from NameAndDescription.tsx

--- src/app/menu/menu-item-details/NameAndDescription.tsx
+++ src/app/menu/menu-item-details/NameAndDescription.tsx
@@ -1,6 +1,5 @@
 import {getTranslation} from "../../../locales/langUtils";
 import {useSelector} from "react-redux";
-import {Labels} from "./Labels.js";
 
 export const NameAndDescription = () => {
     const {menuItem} = useSelector<any, any>(state => state.dishesCategories.view);
@@ -10,7 +9,6 @@ export const NameAndDescription = () => {
         <>
             <div className={`details-name ${!hasBanner ? 'no-banner' : ''}`}>
                 {getTranslation(menuItem.name)}
-                <Labels/>
             </div>
             <div className={'details-description'}>
                 {getTranslation(menuItem.description)}
