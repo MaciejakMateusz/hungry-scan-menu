b914bc829f6eaac91f46952c958114f07cc33da5
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-05-17 08:57:32 +0200

    Created Tooltip rendering on the new info.svg icon in case when menu item has allergens

--- src/app/menu/menu-item-details/NameAndDescription.tsx
+++ src/app/menu/menu-item-details/NameAndDescription.tsx
@@ -1,14 +1,33 @@
 import {getTranslation} from "../../../locales/langUtils";
 import {useSelector} from "react-redux";
+import {Tooltip} from "./Tooltip.tsx";
+import {ReactSVG} from "react-svg";
+import {Allergens} from "./Allergens.tsx";
 
 export const NameAndDescription = () => {
     const {menuItem} = useSelector<any, any>(state => state.dishesCategories.view);
     const hasBanner = menuItem.bestseller || menuItem.new;
+    const hasNoAllergens = !menuItem.allergens || menuItem.allergens.length === 0;
+    const info = document.getElementById('info-icon');
+
+    const renderTooltip = () => {
+        if (hasNoAllergens) return null;
+        return (
+            <Tooltip content={<Allergens/>} appendTo={info || undefined}>
+                <div className={'inline-block'}>
+                    <ReactSVG src={'/theme/icons/info.svg'} id={'info-icon'} className={'info-icon'}/>
+                </div>
+            </Tooltip>
+        );
+    }
 
     return (
         <>
             <div className={`details-name ${!hasBanner ? 'no-banner' : ''}`}>
-                {getTranslation(menuItem.name)}
+                <span>
+                    {getTranslation(menuItem.name)}
+                    {renderTooltip()}
+                </span>
             </div>
             <div className={'details-description'}>
                 {getTranslation(menuItem.description)}
