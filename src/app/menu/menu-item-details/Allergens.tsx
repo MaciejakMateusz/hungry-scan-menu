e4fa841c1e761445ce2bd9a2dcc986d6373ea527
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-05-17 08:57:01 +0200

    Created Allergens component in preparation to render them in a tooltip body

---
+++ src/app/menu/menu-item-details/Allergens.tsx
@@ -0,0 +1,22 @@
+import type {Allergen} from "../../../interfaces/Allergen.ts";
+import {ReactSVG} from "react-svg";
+import {getTranslation} from "../../../locales/langUtils.ts";
+import {useTranslation} from "react-i18next";
+import {useSelector} from "react-redux";
+
+export const Allergens = () => {
+    const {t} = useTranslation();
+    const {menuItem} = useSelector<any, any>(state => state.dishesCategories.view);
+
+    return (
+        <div>
+            <p className={'allergens-tooltip-header'}>{t('canContain')}</p>
+            {menuItem.allergens.map((a: Allergen) => (
+                <div key={a.id} className={'allergen-position'}>
+                    <ReactSVG src={`/theme/icons/${a.iconName}`}/>
+                    <span>{getTranslation(a.name)}</span>
+                </div>
+            ))}
+        </div>
+    );
+}
