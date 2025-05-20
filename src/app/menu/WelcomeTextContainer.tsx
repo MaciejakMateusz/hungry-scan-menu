855a4849be92c38c515683204cfcafc4deebdee1
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-05-20 15:52:51 +0200

    Replaced static welcome message with message that comes from related menu

--- src/app/menu/WelcomeTextContainer.tsx
+++ src/app/menu/WelcomeTextContainer.tsx
@@ -1,7 +1,14 @@
+import {useSelector} from "react-redux";
+import {getTranslation} from "../../locales/langUtils.ts";
+
 export const WelcomeTextContainer = () => {
+    const {menu} = useSelector<any, any>(state => state.main.getMenu);
+
+    if (!menu) return null;
+
     return (
         <div className={'welcome-text-container'}>
-            <span>Hello, friend!</span>
+            <span>{getTranslation(menu.message)}</span>
         </div>
     );
 }
