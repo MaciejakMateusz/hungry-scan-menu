ebcced8103814c5d95d2037cbdbe4f380d1ad88a
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-19 18:22:41 +0100

    Updated LocalizationContainer.tsx

--- src/app/menu/LocalizationContainer.tsx
+++ src/app/menu/LocalizationContainer.tsx
@@ -1,14 +1,16 @@
 import {LocalizationIcon} from "../icons/LocalizationIcon.js";
 import {LanguageSwitcher} from "../../locales/LanguageSwitcher.tsx";
 import {useSelector} from "react-redux";
+import {useParams} from "react-router-dom";
 
 export const LocalizationContainer = () => {
     const {menu} = useSelector<any, any>(state => state.main.getMenu);
+    const {theme} = useParams();
 
     return (
         <div className={'localization-container'}>
             <span className={'flex-centered'}>
-                <LocalizationIcon themeColor={menu?.theme}/>
+                <LocalizationIcon themeColor={theme ? theme : menu?.theme}/>
                 {menu?.restaurant.name}
             </span>
             <LanguageSwitcher/>
