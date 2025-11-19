2397a55acd19e35b970e9c1f51301e5ccf6c64b7
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-19 18:24:23 +0100

    Updated ViewsController.tsx

--- src/app/ViewsController.tsx
+++ src/app/ViewsController.tsx
@@ -4,20 +4,23 @@ import {Menu} from "./menu/Menu";
 import {notifyViewEvent, setMenuItems} from "../slices/statisticsSlice";
 import {useAppDispatch} from "../hooks/hooks.ts";
 import {useEffect} from "react";
+import {useParams} from "react-router-dom";
 
 export const ViewsController = () => {
     const dispatch = useAppDispatch();
     const {menuItem} = useSelector<any, any>(s => s.main.view);
     const {menuItems} = useSelector<any, any>(s => s.statistics.menuItemViews);
+    const {preview} = useParams();
 
     useEffect(() => {
-        if (!menuItem) return;
+        const isPreview = preview === 'true';
+        if (!menuItem || isPreview) return;
 
         if (!menuItems.includes(menuItem.id)) {
             dispatch(setMenuItems([...menuItems, menuItem.id]));
             dispatch(notifyViewEvent({menuItemId: menuItem.id}));
         }
-    }, [dispatch, menuItem, menuItems]);
+    }, [dispatch, menuItem, menuItems, preview]);
 
     return menuItem ? <MenuItemDetails/> : <Menu/>;
 };
