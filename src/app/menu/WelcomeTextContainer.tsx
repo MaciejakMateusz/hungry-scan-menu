0c54d7f314ec176f239509545043d7f9390ca0b0
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-19 18:24:27 +0100

    Updated WelcomeTextContainer.tsx

--- src/app/menu/WelcomeTextContainer.tsx
+++ src/app/menu/WelcomeTextContainer.tsx
@@ -1,14 +1,18 @@
 import {useSelector} from "react-redux";
 import {getTranslation} from "../../locales/langUtils.ts";
+import {useTranslation} from "react-i18next";
+import {useParams} from "react-router-dom";
 
 export const WelcomeTextContainer = () => {
     const {menu} = useSelector<any, any>(state => state.main.getMenu);
+    const {message} = useParams();
+    useTranslation();
 
     if (!menu) return null;
 
     return (
         <div className={'welcome-text-container'}>
-            <span>{getTranslation(menu.message)}</span>
+            <span>{message ? message : getTranslation(menu.message)}</span>
         </div>
     );
 }
