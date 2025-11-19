cd8a5cd211fb0c568d23f8b3db603df119feea69
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-19 18:22:58 +0100

    Updated PriceFooter.tsx

--- src/app/menu/menu-item-details/PriceFooter.tsx
+++ src/app/menu/menu-item-details/PriceFooter.tsx
@@ -1,15 +1,17 @@
 import {formatPrice} from "../../../utils";
 import {useTranslation} from "react-i18next";
 import {useSelector} from "react-redux";
+import {useParams} from "react-router-dom";
 
 export const PriceFooter = () => {
     const {t} = useTranslation();
     const {menuItem} = useSelector<any, any>(state => state.main.view);
     const {menu} = useSelector<any, any>(state => state.main.getMenu);
+    const {theme} = useParams();
 
     return (
         <div className={'details-fixed-footer'}>
-            <div className={'details-price-label'} style={{background: menu?.theme}}>
+            <div className={'details-price-label'} style={{background: theme ? theme : menu?.theme}}>
                         <span className={'details-price-text'}>
                             {t('price')} &nbsp; &#x2022; &nbsp; {formatPrice(menuItem.price)} zł
                         </span>
