da4cc0dc0090b1cd8405bd4405292b46397db09c
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-19 18:21:43 +0100

    Updated Banner.tsx

--- src/app/menu/menu-item/Banner.tsx
+++ src/app/menu/menu-item/Banner.tsx
@@ -1,4 +1,6 @@
 import {ReactSVG} from "react-svg";
+import {useSelector} from "react-redux";
+import {useParams} from "react-router-dom";
 
 type BannerType = {
     name: string;
@@ -6,9 +8,18 @@ type BannerType = {
 }
 
 export const Banner = ({name, iconPath}: BannerType) => {
+    const {menu} = useSelector<any, any>(state => state.main.getMenu);
+    const {bannerIconVisible} = useParams();
+
+    const renderIcon = () => {
+        const shouldShowIcon = bannerIconVisible ? bannerIconVisible === 'true' : menu?.bannerIconVisible;
+        if (!shouldShowIcon) return;
+        return (<ReactSVG src={iconPath} className={'banner-icon'}/>);
+    }
+
     return (
         <div className={'banner-container'}>
-            <ReactSVG src={iconPath} className={'banner-icon'}/>
+            {renderIcon()}
             {name}
         </div>
     );
