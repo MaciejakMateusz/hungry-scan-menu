11257609d817210f1c2cf226c1071104c34e518c
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-24 16:42:52 +0100

    Updated ImageSection.tsx

--- src/app/menu/menu-item-details/ImageSection.tsx
+++ src/app/menu/menu-item-details/ImageSection.tsx
@@ -2,7 +2,6 @@ import {s3BucketUrl} from "../../../apiData";
 import {setMenuItem} from "../../../slices/mainSlice.ts";
 import {useSelector} from "react-redux";
 import {Img} from "react-image";
-import {LoadingSpinner} from "../../icons/LoadingSpinner.js";
 import {useAppDispatch} from "../../../hooks/hooks.ts";
 import {ReactSVG} from "react-svg";
 
@@ -25,7 +24,6 @@ export const ImageSection = ({hasImage}: ImageSectionType) => {
         return (
             <div className={'details-image-container'}>
                 <Img src={`${s3BucketUrl}/${menuItem?.id}.png?t=${menuItem.updated}`}
-                     loader={<LoadingSpinner customContainerStyle={{height: 'calc(100% - 470px)'}}/>}
                      className={'details-image'}
                 />
             </div>
