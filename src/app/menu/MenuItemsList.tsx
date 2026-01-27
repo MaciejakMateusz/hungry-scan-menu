e41c7da5d16cbafaec3ec965520cf471dd401f44
MaciejakMateusz <maciejakmateusz@gmail.com>
2026-01-27 12:22:20 +0100

    Updated MenuItemsList.tsx

--- src/app/menu/MenuItemsList.tsx
+++ src/app/menu/MenuItemsList.tsx
@@ -2,12 +2,15 @@ import {useSelector} from "react-redux";
 import {MenuItemPosition} from "./menu-item/MenuItemPosition.tsx";
 import {useTranslation} from "react-i18next";
 import type {MenuItem} from "../../interfaces/MenuItem.ts";
+import {LoadingSpinner} from "../icons/LoadingSpinner.tsx";
+import type {RootState} from "../../store/store.ts";
 
 export const MenuItemsList = () => {
     const {t} = useTranslation();
     const chosenCategory = useSelector<any, any>(state => state.main.view.category);
     const {filterActive, filteredItems} = useSelector<any, any>(state => state.main.view);
     const {isPending} = useSelector<any, any>(state => state.main.filter);
+    const {isLoading} = useSelector<any, any>((state: RootState) => state.main.getMenu);
     const noPositions = chosenCategory?.menuItems.length === 0;
     const noFilteredItems = filterActive && (!filteredItems || filteredItems?.length === 0) && !isPending;
 
@@ -34,6 +37,7 @@ export const MenuItemsList = () => {
     return (
         <div className={'menu-items-wrapper'}>
             {renderMenuItems()}
+            {(isLoading || isPending) && <LoadingSpinner/>}
             {(noPositions || noFilteredItems) && <p className={'no-positions-text'}>{t('noPositions')}</p>}
         </div>
     );
